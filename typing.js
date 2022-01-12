bd887f7df8b61a5797ec187ce751ce34cc3fed33
vcrcrn <95238312+vcrcrn@users.noreply.github.com>
2022-01-12 15:57:02 -0800

    Add files via upload
    No images yet hehe

---
+++ typing.js
@@ -0,0 +1,18 @@
+// COVER //
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const typing = entry.target.querySelector('.typewriter');
+
+    if (entry.isIntersecting) {
+      typing.classList.add('typing-animation');
+	  return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    typing.classList.remove('typing-animation');
+  });
+});
+
+observer.observe(document.querySelector('.hvrbox-text-papers'));
+observer.observe(document.querySelector('.hvrbox-text-blog'));
+observer.observe(document.querySelector('.hvrbox-text-visualarts'));
